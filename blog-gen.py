d6dda2f0653134fd42fbefcc4c38e6980e7b4f78
Anthony Shaw <anthonyshaw@apache.org>
2020-02-12 14:10:41 +0200

    Add RSS feed

--- blog-gen.py
+++ blog-gen.py
@@ -1,24 +1,41 @@
-from pathlib import Path
 import markdown
 import jinja2
+import glob
+from datetime import datetime
+from email.utils import formatdate, format_datetime  # for RFC2822 formatting
 
+TEMPLATE_FILE = "templates/blog_post_template.html"
+FEED_TEMPLATE_FILE = "templates/rss_feed_template.xml"
+BASE_URL = "https://tonybaloney.github.io/"
 
 def main():
+    posts = glob.glob("blog/*.md")
     extensions = ['extra', 'smarty', 'meta']
     _md = markdown.Markdown(extensions=extensions, output_format='html5')
 
-    templateLoader = jinja2.FileSystemLoader(searchpath='./')
-    templateEnv = jinja2.Environment(loader=templateLoader)
-    TEMPLATE_FILE = '_blog_template.html'
-    template = templateEnv.get_template(TEMPLATE_FILE)
-    
-    for post in Path('./').glob('*.md'):
-        print(f'Rendering {post.stem}')
-        url = f'posts/{post.stem}.html'
-        html = _md.convert(post.read_text())
-        doc = template.render(content=html, baseurl='https://tonybaloney.github.io/', url=url, **_md.Meta)
-        Path(f'./{url}').write_text(doc)
+    loader = jinja2.FileSystemLoader(searchpath="./")
+    env = jinja2.Environment(loader=loader, autoescape=True)
+
+    all_posts = []
+    for post in posts:
+        print("rendering {0}".format(post))
+        url = post.replace(".md", ".html").replace("blog/", "posts/")
+        with open(post) as post_f:
+            html = _md.convert(post_f.read())
+            doc = env.get_template(TEMPLATE_FILE).render(content=html, baseurl=BASE_URL, url=url, **_md.Meta)
+
+        post_html = url
+        with open(post_html, "w") as post_html_f:
+            post_html_f.write(doc)
+        post_date = datetime.strptime(_md.Meta['blog_publish_date'][0], "%B %d, %Y")
+        all_posts.append(dict(**_md.Meta, date=post_date, rfc2822_date=format_datetime(post_date), link="{0}{1}".format(BASE_URL, url)))
+
+    # Order blog posts by date published
+    all_posts.sort(key=lambda item: item['date'], reverse=True)
+    # Make the RSS feed
+    with open("rss.xml", "w") as rss_f:
+        rss_f.write(env.get_template(FEED_TEMPLATE_FILE).render(posts=all_posts, date=formatdate()))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
