3b1c7d0c62bd2a64f57f85b514275d8018c902d5
Anthony Shaw <anthony.p.shaw@gmail.com>
2023-11-15 18:46:43 +1100

    Add new render engine generated home page

--- home-gen.py
+++ home-gen.py
@@ -1,5 +1,12 @@
 from render_engine import Site, Page
 import csv
+from github import Github
+
+# Authentication is defined via github.Auth
+from github import Auth
+
+import os
+
 app = Site()
 
 @app.page
@@ -10,9 +17,28 @@ class Index(Page):
   def __init__(self, *args,  **kwargs):
     self.template_vars = {}
     self.template_vars['podcasts'] = []
+    self.template_vars['projects'] = []
+    github_token = os.getenv("GITHUB_TOKEN", None)
+    if github_token:
+        auth = Auth.Token(github_token)
+        # Public Web Github
+        g = Github(auth=auth)
+        for repo in g.get_user('tonybaloney').get_repos(type="public", sort="stars", direction="desc"):
+            if repo.fork is False and repo.archived is False:
+                self.template_vars['projects'].append({
+                    'name': repo.name,
+                    'url': repo.html_url,
+                    'description': repo.description,
+                    'stars': repo.stargazers_count
+                })
+        # Sort by 'stars'
+        self.template_vars['projects'].sort(key=lambda x: x['stars'], reverse=True)
+        self.template_vars['projects'] = self.template_vars['projects'][:20]
+
+
     with open('pages/podcasts.csv') as podcasts_file:
       reader = csv.DictReader(podcasts_file)
-      for i, row in enumerate(reader):
+      for row in iter(reader):
         self.template_vars['podcasts'].append(row)
 
     super().__init__(*args, **kwargs)
