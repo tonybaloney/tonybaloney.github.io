8122bd86e98a36f7629c9a125e0c5b709cfad0f1
Anthony Shaw <anthony.p.shaw@gmail.com>
2025-08-26 12:07:50 +1000

    Start vector recommendations

--- blog-gen.py
+++ blog-gen.py
@@ -1,3 +1,4 @@
+from typing import TypedDict
 import markdown
 import jinja2
 import glob
@@ -31,6 +32,10 @@ class BootstrapTreeprocessor(Treeprocessor):
         return node
 
 
+def get_closest_matches(title: str) -> tuple[str, str, str]:
+    pass
+
+
 def main():
     posts = glob.glob("blog/*.md")
     extensions = [BootstrapExtension(), 'extra', 'smarty', 'meta', 'tables', 'toc']
@@ -42,20 +47,27 @@ def main():
     all_posts = []
     for post in posts:
         print("rendering {0}".format(post))
-        url = post.replace(".md", ".html").replace("blog/", "posts/")
         with open(post, encoding="utf-8") as post_f:
-            html = _md.convert(post_f.read())
-            _md.Meta['card_image'] = _md.Meta.get('blog_card_image', _md.Meta['blog_header_image'])[0]
-            doc = env.get_template(TEMPLATE_FILE).render(content=html, baseurl=BASE_URL, url=url, **_md.Meta)
-
-        post_html = url
-        with open(post_html, "w", encoding="utf-8") as post_html_f:
-            post_html_f.write(doc)
+            _ = _md.convert(post_f.read()) # We only want metadata this time around
+        url = post.replace(".md", ".html").replace("blog/", "posts/")
+        
+        _md.Meta['card_image'] = _md.Meta.get('blog_card_image', _md.Meta['blog_header_image'])[0]
         post_date = datetime.strptime(_md.Meta['blog_publish_date'][0], "%B %d, %Y")
-        all_posts.append(dict(**_md.Meta, date=post_date, rfc2822_date=format_datetime(post_date), link="{0}{1}".format(BASE_URL, url)))
+        all_posts.append(dict(**_md.Meta, date=post_date, rfc2822_date=format_datetime(post_date), link="{0}{1}".format(BASE_URL, url), rel_html_path=url, rel_md_path=post))
 
     # Order blog posts by date published
     all_posts.sort(key=lambda item: item['date'], reverse=True)
+
+    # render HTML
+    for post in all_posts:
+        with open(post["rel_md_path"], encoding="utf-8") as post_f:
+            html = _md.convert(post_f.read())
+            doc = env.get_template(TEMPLATE_FILE).render(content=html, baseurl=BASE_URL, url=post["rel_html_path"], **post)
+
+        with open(post["rel_html_path"], "w", encoding="utf-8") as post_html_f:
+            post_html_f.write(doc)
+
+
     # Make the RSS feed
     with open("rss.xml", "w", encoding="utf-8") as rss_f:
         rss_f.write(env.get_template(FEED_TEMPLATE_FILE).render(posts=all_posts, date=formatdate()))
